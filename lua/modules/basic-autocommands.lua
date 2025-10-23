0e57f46d089a50a9a9c2b4adb6eb2126835b6d10
anansi <N/A>
2025-10-23 14:40:49 -0400

    Defined some basic autocommands for automatically saving and loading folds across sessions. It need some serious work in terms of automatic cleaning of the viewdir directory. I also need to explicitly handle neotree, as it should attempt to save or load views for that (but it currently is).

--- lua/modules/basic-autocommands.lua
+++ lua/modules/basic-autocommands.lua
@@ -11,4 +11,38 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+-- NOTE: auto_mkview
+-- FIX: adjust so that it doesn't interact with the neotree system
+local auto_mkview = vim.api.nvim_create_augroup('auto_mkview', { clear = true })
+vim.api.nvim_create_autocmd('BufWinLeave', {
+  desc = 'Automatically save folds (and all the other stuff described in the help entry for :mkview) for a buffer when exiting or removing it from a window.',
+  group = auto_mkview,
+  pattern = { '?*' }, -- NOTE: this enforces a non-empty filename, avoiding the problem with empty buffers and pseudo windows (e.g., telescope).
+  command = 'mkview',
+})
+
+vim.api.nvim_create_autocmd('BufWinEnter', {
+  desc = 'Automatically load saved folds (and all the other stuff described in the help entry for :mkview) for the buffer that is entered.',
+  group = auto_mkview,
+  pattern = { '?*' }, -- NOTE: this enforces a non-empty filename, avoiding the problem with empty buffers and pseudo windows (e.g., telescope).
+  command = 'silent! loadview',
+})
+
+-- WARNING: renaming a file won't transfer the views (they will still be stored in the viewdir directory). Therefore, I need to clean the viewdir directory every so often.
+-- FIX: this won't work unless i open it on the first of every month. fix it.
+vim.api.nvim_create_autocmd('VimEnter', {
+  desc = 'Automatically clear the viewdir directory at regular intervals.',
+  group = auto_mkview,
+  callback = function()
+    local currentDayOfTheMonth = os.date '%d'
+    local cleaningRequired = currentDayOfTheMonth == '01' -- clean at the start of every month.
+    if cleaningRequired then
+      os.execute('rm -f ' .. vim.o.viewdir .. '/*')
+    end
+  end,
+})
+
+-- TODO: use autocommands to create C# specific keybinds (e.g., namespace stuff).
+--          see :help lua-guide-autcommands for more info/examples.
+
 -- vim: ts=2 sts=2 sw=2 et
